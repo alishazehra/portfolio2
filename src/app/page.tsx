5aa6ef1aaea41570bb360b8b23a5786ea3e20f09
alisha zehra <alishazehra866@gmail.com>
2024-11-16 22:27:11 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
-
-
 
 
 function Home() {
   return (
     <div>
-      <div className="grid grid-cols-15 grid row "></div>
+      <div className="grid grid-cols-50 grid row  "></div>
       <div className="bg-red-400"> <Link href={"/services"}> Services, </Link> <Link href={"/about"}> About,</Link> <Link href={"/contact"}> Contact</Link>
       </div > <br />
-      <img src="alisha.jpg" height={"50%"}  width={"50%"}/> <br />
+      <h1 style={{textAlign:"center", textDecoration: "underline",  }}>This is Alisha Zehra, Welcome to my Portfolio! </h1>
+      <img src="alisha.jpg" height={"40%"}  width={"40%"}/> <br />
       <button> <Link href={"/contact"}> Contact me</Link></button> <br />
+      
+      
       </div>
   )
 }
